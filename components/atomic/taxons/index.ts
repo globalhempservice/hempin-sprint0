c97347b1ddf2c4c6e37bab398bad386908f3b1ce
SHIGHMAN <me@pauliglesia.com>
2025-09-03 19:06:37 +0200

    fix(taxons): align barrel names (Taxon/Section aliases) to unbreak Netlify build

--- components/atomic/taxons/index.ts
+++ components/atomic/taxons/index.ts
@@ -1,5 +1,12 @@
-// components/taxons/index.ts
-export { default as UniverseHeaderSection }   from './UniverseHeaderSection'
-export { default as UniverseFeaturedSection } from './UniverseFeaturedSection'
-export { default as UniverseExploreSection }  from './UniverseExploreSection'
-export { default as UniverseLowerSection }    from './UniverseLowerSection'
+// components/atomic/taxons/index.ts
+
+export { default as UniverseHeaderTaxon }   from './UniverseHeaderTaxon'
+export { default as UniverseFeaturedTaxon } from './UniverseFeaturedTaxon'
+export { default as UniverseExploreTaxon }  from './UniverseExploreTaxon'
+export { default as UniverseLowerTaxon }    from './UniverseLowerTaxon'
+
+// Back-compat aliases (some files may still import *Section)
+export { default as UniverseHeaderSection }   from './UniverseHeaderTaxon'
+export { default as UniverseFeaturedSection } from './UniverseFeaturedTaxon'
+export { default as UniverseExploreSection }  from './UniverseExploreTaxon'
+export { default as UniverseLowerSection }    from './UniverseLowerTaxon'
