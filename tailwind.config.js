8b778f830f2120ab19536a67496314b94b43cd10
SHIGHMAN <me@pauliglesia.com>
2025-09-07 21:57:53 +0200

    fix(tw): correct content globs to src/* and ensure globals import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{ts,tsx,js,jsx,mdx}",
-    "./src/components/**/*.{ts,tsx,js,jsx,mdx}"
+    './src/app/**/*.{ts,tsx,js,jsx,mdx}',
+    './src/components/**/*.{ts,tsx,js,jsx,mdx}',
+    './src/pages/**/*.{ts,tsx,js,jsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: { brand1: "#6f44ff", brand2: "#27e9b1" }
-    }
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
